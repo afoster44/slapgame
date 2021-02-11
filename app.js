072f203cfba38f55b80f3d8e05174819a3d77b61
ethanmmay <ethaniel.may@gmail.com>
2021-02-11 15:00:05 -0700

    HTML/CSS & Some Functions Built

---
+++ app.js
@@ -0,0 +1,42 @@
+function setPlayer(event) {
+
+}
+
+let target = {
+    dirt: {
+        HP: 100,
+        hits: 0,
+        img: "https://i.unu.edu/media/ourworld.unu.edu-en/article/8634/The-Surprising-Healing-Qualities-of-Dirt.jpg",
+        ground_type: "dirt"
+    },
+    stone: {
+        HP: 150,
+        hits: 0,
+        img: "stone",
+        ground_type: "stone"
+    },
+    coal: {
+        HP: 200,
+        hits: 0,
+        img: "coal",
+        ground_type: "coal"
+    },
+    silicon: {
+        HP: 250,
+        hits: 0,
+        img: "silicon",
+        ground_type: "silicon"
+    }
+}
+
+function shovel() {
+
+}
+
+function pickaxe() {
+    
+}
+
+function drill() {
+    
+}
